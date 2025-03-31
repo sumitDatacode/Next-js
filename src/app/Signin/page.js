41b89aa78917b87a10b57d4adcfed2d90e7f8e4f
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-03-31 22:47:21 +0530

    login page done

--- src/app/Signin/page.js
+++ src/app/Signin/page.js
@@ -3,33 +3,33 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='bg-[#1D1D2A] h-full w-full  flex justify-between'>
-        <div className='relative top-50 left-50'>
-            <div className='border'>
-                  <h1 className='text-2xl tracking-wider font-[900] text-white'><span className='text-[#4A83F6]'>Book</span>Let<span class="w-2 h-2 ml- rounded-full bg-[#4A83F6] ml-0.5 inline-block"></span>
+    <div className='bg-[rgb(29,29,42)] h-full w-full items-center  flex justify-between'>
+        <div className=' relative left-50 w-[400px] ' >
+          
+              <div className='text-center'>
+                   <h1 className='text-[#4A83F6] text-2xl font-[900] tracking-wide '>Book<span className='text-white'>Let</span><span className='bg-[#4A83F6] inline-block w-1 h-1 ml-1'></span></h1>
+                   <p className='ml-[26px]'>by DataCode</p>
+              </div>
 
-                  </h1>
-                   <p className='absolute right-0'>by Datacode</p>
-            </div>
+              <form>
+                <h1 className='mt-8 text-3xl text-center'>Login In To Get Inside</h1>
+                    <br/>
+                <label className='text-sm'>Email</label> <br/>
+                <input type="email" className='border h-10 mt-[4px] rounded-[8px] w-full' />
+                 <br/> <br/>
+                <label className='text-sm'>Password</label> <br/>
+                <input type="password" className='border h-10 mt-[4px] rounded-[8px] w-full' />
 
-             <div>
-                 <h2>Login In To Get Inside</h2>
 
-                 <form>
-                    <label>Email</label>
-                    <input type="email" />
 
-                    <label>Password</label>
-                    <input type="password" />
-                 </form>
-             </div>
-
+                <button className='mt-10 bg-[#4A83F6] w-30 p-2 rounded-full ml-[40%] '>LogIn</button>
+              </form>
         </div>
 
-        <div>
-           <img src="/images/Mask.png" alt="Mask" />
+
+        <div >
+          <img src="/images/Mask.png" className=' object-cover object-top h-[100vh] w-[50vw]'/>
         </div>
-    
     </div>
   )
 }
