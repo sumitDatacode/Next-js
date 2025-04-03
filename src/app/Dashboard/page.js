3c789520f9ce89243b608bdd3b7d7cb2b7634282
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-04-04 01:16:45 +0530

    user dashabord basic done

--- src/app/Dashboard/page.js
+++ src/app/Dashboard/page.js
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import Batman from '../../assets/images/batman.png'
+import Image from 'next/image'
+import Button from '@/components/Button'
 const page = () => {
   return (
-    <div>
-       hi
+    <div className='border '>
+        <div className='border relative w-[500px]'>
+           <Image src={Batman}  alt="batman img" />
+          <div className='absolute bottom-5 w-full'>
+              <p className='text-sm'>Frank Miller</p>
+              <div className='flex '>
+                  <h3 className='text-2xl font-[700]'>Batman: <br/> <span className='whitespace-nowrap'>The Dark Knight</span> </h3>
+                    
+                    <Button/>
+              </div>
+          </div> 
+         
+        </div>
+      
     </div>
   )
 }
