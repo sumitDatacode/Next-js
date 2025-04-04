a8b2551b36d3c98daf271dd6e11edc6a2b01d3ba
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-04-04 23:33:40 +0530

    fix nav styling and add bottom nav

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,33 +1,70 @@
-import React from 'react';
+"use client"
+// import { useRouter } from "next/router";
 import Link from 'next/link';
 import Image from 'next/image';
-
 import Logo from './Logo';
 import DashboardIcon from '../assets/images/Dashboard-icon.png'
 import Bookmark from '../assets/images/bookmark.png'
 import Collection from '../assets/images/collection.png'
+import setting from '../assets/images/setting.png'
+import logOut from '../assets/images/logout.png'
 
 const Sidebar = () => {
+
+    //  const router = useRouter()
+
+     const navList = [
+        {"href" : "/dashboard" , "text" : "Dashboard", "img": DashboardIcon},
+        {"href" : "/mycollection" , "text" : "My Collection", "img": Collection},
+        {"href" : "/favourite" , "text" : "Favourite", "img": Bookmark}
+     ]
+
+     const bottomNavList = [
+        {"href" : "/setting" , "text" : "Settings", "img": setting},
+        {"href" : "/logout" , "text" : "Log Out", "img": logOut},
+       
+     ]
   return (
    <div className='flex'>
   
-    <div className=' fixed  left-0 top-0 h-full  w-[350px] flex  border-r border-[#282837]  flex-col pt-[40px] items-start  '>
-    <div className="pl-[50px]" >
-       <Logo />
-    </div>
-   
-    <div className='flex gap-4 text-xl mt-[100px] p-4 pl-[50px] w-[300px]  rounded-3xl bg-[#4A83F6]'>
-        <Image src={DashboardIcon} alt="Dashboard icon"/>
-        <Link href="/Dashboard" >Dashboard</Link>
-    </div>
-    <div className='flex gap-4 text-xl mt-5 p-4 pl-[50px]'>
-        <Image src={Collection} alt="Dashboard icon"/>
-        <Link href="/Dashboard" >My Collection</Link>
-    </div>
-    <div className='flex gap-4 text-xl  mt-5 p-4 pl-[50px]'>
-        <Image src={Bookmark} alt="Dashboard icon"/>
-        <Link href="/Dashboard" >Favourite</Link>
-    </div>
+    <div className=' fixed left-0 top-0 h-full  w-[350px] flex  border-r border-[#282837] justify-between flex-col pt-[40px] items-start  '>
+     <div>
+                <div className="pl-[50px]" >
+                <Logo />
+                </div>
+            
+                <div className='mt-[60px]'>
+
+                {
+                        navList.map((item)=>{
+                            return (
+                                <div key={item.href} className='flex gap-4  w-[18rem]  rounded-2xl  text-xl mt-2  p-3 pl-[50px]'>
+                                    <Image src={item.img} className='object-contain' alt={item.text}/>
+                                    <Link href={item.href}>{item.text} </Link>
+                                </div>    
+                            )
+                        })
+                    }
+
+                </div>
+     </div>
+    
+
+     <div className='w-[18rem] mb-3'>
+        {
+                bottomNavList.map((item)=>{
+                    return (
+                        <div key={item.href} className='flex gap-4 rounded-2xl   text-xl mt-1 p-2 pl-[50px]'>
+                            <Image src={item.img} className='object-contain' alt={item.text}/>
+                            <Link href={item.href}>{item.text} </Link>
+                        </div>    
+                    )
+                })
+            }
+     </div>
+
+            
+    
  </div>
 
 
