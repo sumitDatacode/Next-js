3c6b48a84f4f4880881ef82c23ef41294d6903ee
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-04-12 23:14:20 +0530

    make comics responsive

--- src/components/Comics.jsx
+++ src/components/Comics.jsx
@@ -21,12 +21,12 @@ const Comics = ({Heading}) => {
     <div className='mt-10 mb-10'>
     <h2 className='text-2xl font-bold'>{Heading}</h2>
 
-    <div className='flex flex-wrap gap-6 justify-between w-[90%] '>
+    <div className='flex flex-wrap gap-6 justify-center md:justify-between w-[90%] '>
     {
         comics.map((comic, index)=>{
             return (
-                     <div key={index} className='mt-10'>
-                            <Image src={comic.image} className='rounded-2xl' alt={comic.title} />
+                     <div key={index} className='mt-10 '>
+                            <Image src={comic.image} className='rounded-2xl ' alt={comic.title} />
                             <h2 className='text-[#4A83F6]  mt-1 font-[700] mb-1'>{comic.title}</h2>
                             <h2>{comic.name}</h2>
                       </div>
