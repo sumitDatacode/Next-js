5f19f88949bfe794f885a0af422c23b52259a59a
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-04-13 23:35:26 +0530

    form setup

--- src/components/pages/CreateBook/CreateBookPage.jsx
+++ src/components/pages/CreateBook/CreateBookPage.jsx
@@ -9,18 +9,18 @@ const CreateBookPage = () => {
 
              <h2 className='m-4 text-2xl font-[500]'>Create a Book</h2>
 
-             <form className='w-[70%]'> 
+             <form className='w-[70%]' onSubmit={formHandler}> 
                     {/* <h1 className='mt-8 text-3xl text-center font-[500]'>{heading}</h1> */}
                         <br/>
                     <label className='text-sm'>Book Title</label> <br/>
-                    <input type="text" className='border  h-10 mt-[4px] rounded-[8px] w-[100%]' />
+                    <input type="text" onChange={inputCHange} className='border  h-10 mt-[4px] rounded-[8px] w-[100%]' />
                 
                     <br/> <br/>
                     <label className='text-sm'>Book Auther</label> <br/>
-                    <input type="text" className='border h-10 mt-[4px] rounded-[8px] w-[100%]' />
+                    <input type="text" onChange={inputCHange}  className='border h-10 mt-[4px] rounded-[8px] w-[100%]' />
                     <br/> <br/>
                     <label className='text-sm'>No of chapters</label> <br/>
-                    <input type="text" className='border h-10 mt-[4px] rounded-[8px] w-[100%]' />
+                    <input type="text"  onChange={inputCHange}  className='border h-10 mt-[4px] rounded-[8px] w-[100%]' />
                 <div className='text-center mt-6'>
                 <Button text="Create book"/>
                 
