9d06e518e01f48ee281acabacefa50c8fdc993b9
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-04-15 23:39:34 +0530

    cursor for logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <div className='w-[140px] text-center'>
+    <div className='w-[140px] text-center cursor-pointer'>
               
                    <h1 className='text-[#4A83F6] text-2xl font-[900] tracking-tighter '>Book<span className='text-white'>Let</span><span className='bg-[#4A83F6] inline-block w-1 h-1 ml-1'></span></h1>
                     <p className='ml-[30px] text-sm font-bold'>by DataCode</p>
