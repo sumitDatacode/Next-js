ce4200bef1a0183d66ce9a55ff9965b37d6c8b7c
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-04-15 22:31:19 +0530

    set onchange prooper

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,19 @@
+"use client"
 import React from 'react'
 import Button from './Button'
 import Input from './Input'
 const Form = ({heading}) => {
+
+
+  const inputCHange = () =>{
+     alert("hh")
+  }
   return (
     <form  className='w-[70%]'>
     <h1 className='m-8 text-3xl text-center font-[500]'>{heading}</h1>
         
-    <Input type="email" label="Email"/>
-     
-     <Input type="password" label="Password"/>
-     
+    <Input type="email" name="email" label="Email" onChange={inputCHange} />
+    <Input type="password" name="password" label="Password" onChange={inputCHange} />
      
      <div className='text-center mt-6'>
        <Button text="Log in"/>
