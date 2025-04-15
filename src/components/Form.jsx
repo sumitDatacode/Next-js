0192e4e47bf95dc2b5e6fc03b699e05cd3c46caf
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-04-15 22:49:51 +0530

    login page form work perfect

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,31 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 import Input from './Input'
 const Form = ({heading}) => {
+ const [formdata, setFormData] = useState()
 
 
-  const inputCHange = () =>{
-     alert("hh")
-  }
+ const inputCHange = (e) =>{
+     const {name, value} = e.target
+       
+     setFormData((prev)=> ({
+      ...prev,
+       [name]:value
+     }))
+ }
+
+
+
+
+ const formSubmit = (e) =>{
+  e.preventDefault();
+  console.log('formsubmitted', formdata)
+ }    
+
+
   return (
-    <form  className='w-[70%]'>
+    <form  className='w-[70%]' onSubmit={formSubmit}>
     <h1 className='m-8 text-3xl text-center font-[500]'>{heading}</h1>
         
     <Input type="email" name="email" label="Email" onChange={inputCHange} />
