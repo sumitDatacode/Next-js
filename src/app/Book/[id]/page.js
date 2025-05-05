ccb3e2f82d7bd31ce3f3ced1b23171d84a34ad6e
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-05-06 00:14:18 +0530

    dynamic book route

--- src/app/Book/[id]/page.js
+++ src/app/Book/[id]/page.js
@@ -1,10 +1,11 @@
+"use client"
 import React from 'react'
-
-const page = () => {
+import BookPage from '@/components/pages/Book'
+const page = ({params}) => {
   return (
     <div>
-      hi
-    </div>
+       <BookPage id={params.id}/>
+     </div>
   )
 }
 
--- src/components/pages/Book/BookPage.jsx
+++ src/components/pages/Book/BookPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import RightLoader from '@/components/RightLoader'
 import Table from '@/components/Table';
-const BookPage = () => {
+const BookPage = ({id}) => {
     const smallHeading = {
         title: 'Started:',
         time: '5 Days Ago'
@@ -15,7 +15,7 @@ const BookPage = () => {
 
             <div className='w-[640px] '>
                 <div className='text-3xl sm:text-4xl md:text-7xl font-[700]'>
-                    <h1>Batman:</h1>
+                    <h1>Batman:{id}</h1>
                     <h1 className='md:whitespace-nowrap'>The Dark Knight</h1>
                     <p className='text-[1.1rem]  font-[200]  text-justify  md:text-lg md:font-[400] mt-8'>A book description is a brief summary of a book's plot, characters, and themes. It helps readers get a sense of what the book is about. Book descriptions are often found on the back cover of the book, or on retailer websites. </p>
                 </div>
